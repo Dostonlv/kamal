f0859166fc1ecba6b0381aaddfbd02f03809e930
Doston <doston@icloud.com>
2025-05-17 04:31:13 +0500

    test

--- app.go
+++ app.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", helloServer)
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8080", nil)
 }
 
 func helloServer(w http.ResponseWriter, r *http.Request) {
